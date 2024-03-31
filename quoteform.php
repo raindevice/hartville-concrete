4c0374230243f0d2e9b4eb5054febce38dab5218
bekindrwd <rywilldav@gmail.com>
2024-03-31 09:58:46 -0400

    add mailHeader and recipient to quoteform.php

--- quoteform.php
+++ quoteform.php
@@ -5,9 +5,9 @@ $email = $_POST['email'];
 $phone = $_POST['phone'];
 $message = $_POST['message'];
 
+$mailHeader = "From:".$fname." ".$lname."<".$email.">\r\n";
 
-
-
+$recipient = "hartvilleconcrete@gmail.com";
 
 
 
